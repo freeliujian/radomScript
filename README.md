104baeda2a4242b91457d0167fd3ed6a05546d56
littleforest <670177821@qq.com>
2022-10-09 17:57:35 +0800

    feat: 更新脚本

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # radomScript
 公司随机英语日报
+
+## 使用
+```
+npm link
+```
+
+ex: 
+```
+rdomScript 刘坚
+```
+会剔除掉刘坚
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+function translateList(onLeaveColleagues) {
+  let colleagues = [
+    "陈侃",
+    "文旺",
+    "张一行",
+    "张基琳",
+    "刘坚",
+    "李浩",
+    "闫磊刚",
+    "王海飞",
+  ];
+
+  if (onLeaveColleagues.length !== 0 && onLeaveColleagues.length < 4) {
+    colleagues = colleagues.filter((name) => !onLeaveColleagues.includes(name));
+  }
+
+  const arr = JSON.parse(JSON.stringify(colleagues));
+
+  const randomArray = [];
+
+  colleagues.forEach(() => {
+    let index = Math.floor(Math.random() * arr.length);
+
+    randomArray.push(arr[index]);
+
+    arr.splice(index, 1);
+  });
+  
+  let str = "";
+  str += "-----------------------------\n";
+  str += new Date().toLocaleDateString();
+  str +="翻译者： 被翻译者\n";
+
+  randomArray.forEach((item, i) =>
+    str +=`${item}： ${randomArray[i + 1] || randomArray[0]}\n`
+  );
+  str += "-----------------------------";
+  console.log(str);
+}
+
+translateList([process.argv[process.argv.length - 1]]);
